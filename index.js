a4f4bd4b1a9bac63640f0efdd9902938ba0d26d8
Rasel Kabir <rasel.kabir@shiplux.com>
2022-06-09 22:34:34 +0600

    Select and Input ID and replace

---
+++ index.js
@@ -0,0 +1,10 @@
+//Text To Number
+
+myButton = document.getElementById("year"); //searches for and detects the input element from the 'year' id
+myButton.type = "number"; // value will be change as Number
+
+
+//Number To text
+
+myButton = document.getElementById("year"); //searches for and detects the input element from the 'year' id
+myButton.type = "text"; //Number will be change as text
